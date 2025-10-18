bd5134a946954867fdb2880d23ef0696397d220d
mayyadow <mayadowman2024@gmail.com>
2025-10-17 22:29:50 -0400

    added exception

--- src/main/java/dogapi/Main.java
+++ src/main/java/dogapi/Main.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws BreedFetcher.BreedNotFoundException {
         String breed = "hound";
         BreedFetcher breedFetcher = new CachingBreedFetcher(new BreedFetcherForLocalTesting());
         int result = getNumberOfSubBreeds(breed, breedFetcher);
@@ -23,7 +23,7 @@ public class Main {
      * @return the number of sub breeds. Zero should be returned if there are no sub breeds
      * returned by the fetcher
      */
-    public static int getNumberOfSubBreeds(String breed, BreedFetcher breedFetcher) {
+    public static int getNumberOfSubBreeds(String breed, BreedFetcher breedFetcher) throws BreedFetcher.BreedNotFoundException {
         List<String> breeds = breedFetcher.getSubBreeds(breed);
 
         if (breeds.isEmpty()) {return 0;}
